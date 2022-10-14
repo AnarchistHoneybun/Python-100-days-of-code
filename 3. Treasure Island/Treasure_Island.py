3ceb1f9b7a53bf1058fdf6ada96a85f56d0f6e5b
Nirvana <thatonebipanda@gmail.com>
2022-10-14 22:06:50 +0530

    Update Treasure_Island.py

--- 3. Treasure Island/Treasure_Island.py
+++ 3. Treasure Island/Treasure_Island.py
@@ -20,13 +20,13 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/_____ /
 *******************************************************************************
 ''')
-print("Welcome to Treasure Island.")
+print("!!!Welcome to Treasure Island!!!")
 print("Your mission is to find the treasure.") 
 
 
-choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+choice1 = input("You\'re at a cross road. Where do you want to go? Type \"left\" or \"right\" \n").lower()
 if choice1 == "left":
-  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  choice2 = input("You\'ve come to a lake. There is an island in the middle of the lake. Type \"wait\" to wait for a boat. Type \"swim\" to swim across. \n").lower()
   if choice2 == "wait":
     choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
     if choice3 == "red":
